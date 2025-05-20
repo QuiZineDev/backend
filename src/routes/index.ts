00fbdb1eda350a016130ef624bbe3efedd969bad
arussac <antoninrussac1@gmail.com>
2025-05-20 12:39:45 +0200

    feat: add history and profile routes with corresponding controllers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,11 @@ import libraryRouter from "./library"
 import quizRouter from "./quiz"
 import exploreRouter from "./explore"
 import recentRouter from "./recent"
+import searchRouter from "./search"
+import historyRouter from "./history"
+import loginRouter from "./login"
+import profileRouter from "./profile"
+
 
 const router = Router()
 
@@ -10,5 +15,9 @@ router.use("/library", libraryRouter)
 router.use("/quiz", quizRouter)
 router.use("/explore", exploreRouter)
 router.use("/recent", recentRouter)
+router.use("/search", searchRouter)
+router.use("/history", historyRouter)
+router.use("/login", loginRouter)
+router.use("/profile", profileRouter)
 
 export default router
