b5feab53d82b79887edbb0c4ff4ce1eed0651af4
raphael.thieffry <raphael.thieffry@gmail.com>
2025-05-20 10:28:35 +0200

    nettoyage

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,14 +1,14 @@
 import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+// import { DataSource } from "typeorm"
+//import { User } from "./entity/User"
 
-export const AppDataSource = new DataSource({
-    type: "postgres",
-    url: process.env.DATABASE_URL || "https://supabase-quiz.kerboul.me",
-    ssl: true,
-    synchronize: false, // Plus prudent avec Supabase de ne pas synchroniser automatiquement
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+// export const AppDataSource = new DataSource({
+//     type: "postgres",
+//     url: process.env.DATABASE_URL || "https://supabase-quiz.kerboul.me",
+//     ssl: true,
+//     synchronize: false, // Plus prudent avec Supabase de ne pas synchroniser automatiquement
+//     logging: false,
+//     entities: [User],
+//     migrations: [],
+//     subscribers: [],
+// })
