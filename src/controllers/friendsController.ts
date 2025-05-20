0e5aae5c4fa0b6ffe0f0436b9e21cf2bf2304383
arussac <antoninrussac1@gmail.com>
2025-05-20 15:46:16 +0200

    fix: correct user ID extraction in friend request handling functions

--- src/controllers/friendsController.ts
+++ src/controllers/friendsController.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express"
-import { linkWithSomeone,findSpecificAmisById,Amis, findAmisById } from "../models/Amis"
+import { linkWithSomeone,findSpecificAmisById,findAmisById } from "../models/Amis"
 import { findUserById } from "../models/User"
 import { didIAsked,createFriendRequest,deleteFriendRequest } from "../models/FriendRequest"
 
 export const askFriend = (req: Request, res: Response) => {
   const idValidator = Number(req.params.idValidator) as (number | null)
-  const currentIdUser = req.user
+  const currentIdUser = req.user.id
 
   if (!idValidator) {
       return res.status(400).json({ error: "idValidator is required" })
@@ -36,7 +36,7 @@ export const askFriend = (req: Request, res: Response) => {
 export const acceptFriend = (req: Request, res: Response) => {
   
   const idRequestor = Number(req.params.idRequestor) as (number | null)
-  const currentIdUser = req.user
+  const currentIdUser = req.user.id
 
   if (!idRequestor) {
       return res.status(400).json({ error: "idRequestor is required" })
@@ -52,7 +52,7 @@ export const acceptFriend = (req: Request, res: Response) => {
 }
 
 export const refuseFriend = (req: Request, res: Response) => {
-  deleteFriendRequest(req.user, Number(req.params.idRequestor)).then((friendRequest) => {
+  deleteFriendRequest(req.user.id, Number(req.params.idRequestor)).then((friendRequest) => {
     if (!friendRequest) {
       res.json({ message: `Friend request deleted` })
     }
@@ -60,7 +60,7 @@ export const refuseFriend = (req: Request, res: Response) => {
 }
 
 export const getFriends = (req: Request, res: Response) => {
-  const currentIdUser = req.user
+  const currentIdUser = req.user.id
   findAmisById(currentIdUser).then((amis) => {
     if (!amis) {
       res.status(404).json({ error: "No friends found" })
