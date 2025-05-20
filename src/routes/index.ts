2b5069af8654ea956af01955054fe876612fcb13
emma <emma.degot@gmail.com>
2025-05-20 12:28:59 +0200

    feat: add recent quizzes route and controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express"
 import libraryRouter from "./library"
 import quizRouter from "./quiz"
 import exploreRouter from "./explore"
+import recentRouter from "./recent"
 
 const router = Router()
 
 router.use("/library", libraryRouter)
 router.use("/quiz", quizRouter)
 router.use("/explore", exploreRouter)
+router.use("/recent", recentRouter)
 
 export default router
