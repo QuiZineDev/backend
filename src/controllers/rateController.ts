2fa3cacde0562af368338067223bf70e10af1662
emma <emma.degot@gmail.com>
2025-05-20 17:33:15 +0200

    feat: implement grading functionality with createGrade model and rate controller

--- src/controllers/rateController.ts
+++ src/controllers/rateController.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from "express"
+import { createGrade } from "../models/Grade"   
 
 export const rate = (req: Request, res: Response) => {
-  res.json({ message: `rate a labelisabel element` })
+    const { idLabelisable, idUser, grade } = req.body
+    const newGrade = createGrade(idLabelisable, idUser, grade)
+    res.json({ message: `Rated quiz ${idLabelisable} with grade ${grade}`, newGrade })
+    }
+
+export const getRate = (req: Request, res: Response) => {
+    const { idLabelisable } = req.params
+    res.json({ message: `Rating for quiz ${idLabelisable}`, rating: 4.5 })
 }
+
