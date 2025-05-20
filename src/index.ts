584d87d77f29a6dc47516409b04b115827416d8a
raphael.thieffry <raphael.thieffry@gmail.com>
2025-05-20 10:40:43 +0200

    correction caca

--- src/index.ts
+++ src/index.ts
@@ -26,10 +26,6 @@ import express from "express"
 import routes from "./routes"
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "https://supabase-quiz.kerboul.me"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE"
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
 import { supabase } from './supabaseClient';
 
 /**
