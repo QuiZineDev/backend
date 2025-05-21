bb31365803d2270692093994c0345df250075afc
arussac <antoninrussac1@gmail.com>
2025-05-21 09:53:53 +0200

    feat: implement CreateQuiz controller with user ID and content handling

--- src/controllers/createQuizController.ts
+++ src/controllers/createQuizController.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express"
+import { Quiz, createQuiz} from "../models/Quiz"
 
 export const CreateQuiz = (req: Request, res: Response) => {
+  const userId = req.user.id
+  const content = req.body
+  createQuiz(content.nom, content.picture, content.isprivate, userId)
   res.json({ message: `Create new quiz` })
 }
